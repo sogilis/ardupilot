e7fc15d57104c9d1d4e152c44300c3b55c5503d0
MousS <eric@sogilis.com>
2014-06-12 10:39:03 +0200

    CONDITION_YAW test added

--- Tools/ArduCopterTestUnit/gcs_mavlink_test_unit.cpp
+++ Tools/ArduCopterTestUnit/gcs_mavlink_test_unit.cpp
@@ -15,6 +15,15 @@ mavlink_system_t mavlink_system = {7,1,0,0,0,0};
 #include "stubs/gcs_mavlink_stub.h"
 #include "catch.hpp"
 
+
+#define PARAM1_OFFSET 0
+#define PARAM2_OFFSET 4
+#define PARAM3_OFFSET 8
+#define PARAM4_OFFSET 12
+#define PARAM5_OFFSET 16
+#define PARAM6_OFFSET 20
+#define PARAM7_OFFSET 24
+
 // Function Stubs
 
 uint32_t millis() {return 0;}
@@ -38,6 +47,8 @@ void init_disarm_motors() { }
 uint8_t mavlink_compassmot(mavlink_channel_t chan) {return 3;}
 void check_usb_mux(void) {} ;
 static bool do_guided(const AP_Mission::Mission_Command& cmd) {return true;}
+// Stubbed method
+static void set_auto_yaw_look_at_heading(float angle_deg, float turn_rate_dps, uint8_t relative_angle); // control_auto
 
 
 // Variables Stubs
@@ -90,6 +101,14 @@ uint8_t compid;  ///< ID of the message sender component
 uint8_t msgid;   ///< ID of message in payload
 uint64_t payload64[(MAVLINK_MAX_PAYLOAD_LEN+MAVLINK_NUM_CHECKSUM_BYTES+7)/8];
 
+class Control_Auto_Stub {
+public:
+	float   angle_deg;      // target angle in degrees (0=north, 90=east)
+	float   turn_rate_dps;  // turn rate in degrees / second (0=use default)
+	uint8_t relative_angle; // 0 = absolute angle, 1 = relative angle
+};//Control_Auto_Stub
+static Control_Auto_Stub control_auto_stub;
+
 static union {
     struct {
         uint8_t home_is_set         : 1; // 0
@@ -125,10 +144,19 @@ static struct {
 uint8_t result [11];
 int result_index = 0;
 
+
 void comm_send_ch(mavlink_channel_t chan, uint8_t ch) {
 	result [result_index] = ch;
 	result_index++;
-}
+}//comm_send_ch
+
+
+static void set_auto_yaw_look_at_heading(float angle_deg, float turn_rate_dps, uint8_t relative_angle) {
+	control_auto_stub.angle_deg      = angle_deg;
+	control_auto_stub.turn_rate_dps  = turn_rate_dps;
+	control_auto_stub.relative_angle = relative_angle;
+}//set_auto_yaw_look_at_heading
+
 
 class Fixture {
 
@@ -150,7 +178,7 @@ public:
 	GCS_MAVLINK       sut;
 	uint8_t 		  tab[4];
 
-	void setup (float initial_speed_in_cm_per_s) {
+	void setup_speed_test (float initial_speed_in_cm_per_s) {
 		reset();
 		wp_nav.speed_in_cms = initial_speed_in_cm_per_s;
 		uint16_t unused_checksum = 0;
@@ -177,40 +205,136 @@ public:
 		payload_ptr [5]  = tab [1];
 		payload_ptr [6]  = tab [2];
 		payload_ptr [7]  = tab [3];
-	}
+	}//setup_speed_test
+
+	void setup_yaw_test (float param1, float param2, float param4) {
+		reset();
+		uint16_t unused_checksum = 0;
+		uint8_t  unused_protocol_magic = 0;
+		uint8_t  unused_payload_lenght = 0;
+		uint8_t  unused_sequence = 0;
+		uint8_t  unused_sysid = 0;
+		uint8_t  unused_compid = 0;
+		uint8_t  command_long_msg_ig = 0x4C;
+		msg =
+		{
+				unused_checksum,
+				unused_protocol_magic,
+				unused_payload_lenght,
+				unused_sequence,
+				unused_sysid,
+				unused_compid,
+				command_long_msg_ig, {}
+		};
+		uint8_t* payload_ptr = (uint8_t*) msg.payload64;
+		float* param;
+		param = (float*)&payload_ptr[PARAM1_OFFSET];
+		*param = param1;
+		param = (float*)&payload_ptr[PARAM2_OFFSET];
+		*param = param2;
+		param = (float*)&payload_ptr[PARAM4_OFFSET];
+		*param = param4;
+		payload_ptr [28] = 0x73;    // Command = 115 for CONDITION_YAW
+		payload_ptr [29] = 0x00;
+		// Parameters
+
+	}//setup_yaw_test
 
 	void exercize (void) {
 		sut.handleMessage(&msg);
-	}
+	}//exercize
 
-	void check (float expected_speed, uint8_t status) {
+	void check_speed_test (float expected_speed, uint8_t status) {
 		REQUIRE (get_result(6) == 0xB2);  				 // Check Mav Cmd = 178 (DO_CHANGE_SPEED)
 		REQUIRE (get_result(7) == 0);
 		REQUIRE (get_result(8) == status);   			 // Accepted and Executed
 		REQUIRE (wp_nav.speed_in_cms == expected_speed); // Check Speed Value Is Not Changed
-	}
+	}//check_speed_test
 
+	void check_yaw_test (uint8_t status, float angle_deg, float turn_rate_dps, uint8_t relative_angle) {
+		REQUIRE (get_result(6)                    == 0x73);  	     // Check Mav Cmd = 115 (CONDITION_YAW)
+		REQUIRE (get_result(7)                    == 0);
+		REQUIRE (get_result(8)                    == status);   	 // Accepted and Executed
+		REQUIRE (control_auto_stub.angle_deg      == angle_deg);
+		REQUIRE (control_auto_stub.turn_rate_dps  == turn_rate_dps);
+		REQUIRE (control_auto_stub.relative_angle == relative_angle);
+	}//check_speed_test
 };
 
-TEST_CASE("Invalid Case", "COMMAND_LONG | DO_CHANGE_SPEED") {
+TEST_CASE("Speed Invalid Case", "COMMAND_LONG | DO_CHANGE_SPEED") {
 	Fixture fixture;
 	fixture.tab[0] = 0x00;
 	fixture.tab[1] = 0x00;
 	fixture.tab[2] = 0x40;
 	fixture.tab[3] = 0xC0;		//-3.0 in m/s
-	fixture.setup(3.0);
+	fixture.setup_speed_test(3.0);
 	fixture.exercize();
-	fixture.check(3.0, 4);		// Command failed - Speed Value Is Not Changed
+	fixture.check_speed_test(3.0, 4);		// Command failed - Speed Value Is Not Changed
 }
 
-TEST_CASE("Nominal Case", "COMMAND_LONG | DO_CHANGE_SPEED") {
+TEST_CASE("Speed Nominal Case", "COMMAND_LONG | DO_CHANGE_SPEED") {
 	Fixture fixture;
 	fixture.tab[0] = 0x00;
 	fixture.tab[1] = 0x00;
 	fixture.tab[2] = 0x40;
 	fixture.tab[3] = 0x40;		//3.0 in m/s
-	fixture.setup(0.0);
+	fixture.setup_speed_test(0.0);
 	fixture.exercize();
-	fixture.check(300.0, 0);	// Command is Accepted and Executed - Speed Value Is Changed
+	fixture.check_speed_test(300.0, 0);	// Command is Accepted and Executed - Speed Value Is Changed
+}
+
+TEST_CASE("Yaw Nominal Case (absolute)", "COMMAND_LONG | CONDITION_YAW") {
+	Fixture fixture;
+
+	control_auto_stub.angle_deg      = 0;
+	control_auto_stub.turn_rate_dps  = 0;
+	control_auto_stub.relative_angle = 0xFF;
+	fixture.setup_yaw_test(180, 1, 0);
+	fixture.exercize();
+	fixture.check_yaw_test(0, 180, 1, 0);	// Command is accepted and executed - Yaw value is changed
+}
+
+TEST_CASE("Yaw Nominal Case (relative)", "COMMAND_LONG | CONDITION_YAW") {
+	Fixture fixture;
+
+	control_auto_stub.angle_deg      = 0;
+	control_auto_stub.turn_rate_dps  = 0;
+	control_auto_stub.relative_angle = 0xFF;
+	fixture.setup_yaw_test(90, 2, 1);
+	fixture.exercize();
+	fixture.check_yaw_test(0, 90, 2, 1);	// Command is accepted and executed - Yaw value is changed
+}
+
+TEST_CASE("Yaw Error Case (Yaw < 0)", "COMMAND_LONG | CONDITION_YAW") {
+	Fixture fixture;
+
+	control_auto_stub.angle_deg      = 0;
+	control_auto_stub.turn_rate_dps  = 0;
+	control_auto_stub.relative_angle = 0xFF;
+	fixture.setup_yaw_test(-0.01, 3, 0);
+	fixture.exercize();
+	fixture.check_yaw_test(4, 0, 0, 255);	// Command failed - Yaw value remains unchanged
+}
+
+TEST_CASE("Yaw Error Case (Yaw > 360)", "COMMAND_LONG | CONDITION_YAW") {
+	Fixture fixture;
+
+	control_auto_stub.angle_deg      = 0;
+	control_auto_stub.turn_rate_dps  = 0;
+	control_auto_stub.relative_angle = 0xFF;
+	fixture.setup_yaw_test(360.01, 0.5, 1);
+	fixture.exercize();
+	fixture.check_yaw_test(4, 0, 0, 255);	// Command failed - Yaw value remains unchanged
+}
+
+TEST_CASE("Yaw Error Case (Angle mode neither absolute nor relative)", "COMMAND_LONG | CONDITION_YAW") {
+	Fixture fixture;
+
+	control_auto_stub.angle_deg      = 0;
+	control_auto_stub.turn_rate_dps  = 0;
+	control_auto_stub.relative_angle = 0xFF;
+	fixture.setup_yaw_test(270, 1, 2);
+	fixture.exercize();
+	fixture.check_yaw_test(4, 0, 0, 255);	// Command failed - Yaw value remains unchanged
 }
 
